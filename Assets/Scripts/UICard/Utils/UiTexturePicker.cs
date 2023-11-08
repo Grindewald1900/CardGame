3952980456e38cb68b8da8df276da3839ba17376
Grindewald1900 <17788108425@163.com>
2023-11-07 23:16:11 -0500

    Init commit

---
+++ Assets/Scripts/UICard/Utils/UiTexturePicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Extensions;
+using UnityEngine;
+
+namespace Tools.UI.Card
+{
+    /// <summary>
+    ///     Picks a Texture randomly when it Awakes.
+    /// </summary>
+    public class UiTexturePicker : MonoBehaviour
+    {
+        // [SerializeField] Sprite[] Sprites;
+        Sprite[] Sprites;
+        [SerializeField] SpriteRenderer MyRenderer { get; set; }
+
+        void Awake()
+        {
+            Sprites = Resources.LoadAll<Sprite>("CharacterTextures");
+            MyRenderer = GetComponent<SpriteRenderer>();
+
+            if (Sprites.Length > 0)
+                MyRenderer.sprite = Sprites.ToList().RandomItem();
+        }
+    }
+}
